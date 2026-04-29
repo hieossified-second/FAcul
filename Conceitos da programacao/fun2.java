c9e479591e1f6f7cf59c00aa809992aa9325e29b
Kevin <kevindasilvalima2008@gmail.com>
2026-04-29 11:08:21 -0300

    Add files via upload

---
+++ Conceitos da programacao/fun2.java
@@ -0,0 +1,18 @@
+
+public class fun2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("antes de chamar o metodo");
+        imprimirInformacoes("Like a hacker");
+        System.out.println("depois de chamar o metodo");
+    }
+
+    public static void imprimirInformacoes(String mensagem) {
+        System.out.println("========================================");
+        System.out.println("Logica de Programacao");
+        System.out.println("Aprendendo a programar");
+        System.out.println(mensagem);
+
+    }
+}
