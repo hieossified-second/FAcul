49ce0f6dcd8afed4bfa19bf0404ffb1bab45157b
Kevin <kevindasilvalima2008@gmail.com>
2026-07-09 13:03:31 -0300

    Update testandogit.java

--- src/testandogit.java
+++ src/testandogit.java
@@ -2,6 +2,7 @@ public class testandogit {
     public static void main(String[] args) {
         System.out.println("Solo foi pro github kkkkkk");
         laele();
+        System.out.println("Work");
     }
 
     public static void laele() {
