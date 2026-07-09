d038cc321278799c2da39d99801220108d4b74ad
  <kevindasilvalima2008@gmail.com>
2026-07-09 12:59:56 -0300

    Index

---
+++ src/testandogit.java
@@ -0,0 +1,10 @@
+public class testandogit {
+    public static void main(String[] args) {
+        System.out.println("Solo foi pro github kkkkkk");
+        laele();
+    }
+
+    public static void laele() {
+        System.out.println("Opaxxx");
+    }
+}
